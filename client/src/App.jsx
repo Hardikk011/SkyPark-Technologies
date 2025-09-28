c44df4bd74f160bcd7082b7104f5faa85df25487
Hardikk011 <hvp5694@gmail.com>
2025-09-28 20:22:15 +0530

    remove the contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-import Contact from "@/pages/contact";
+// import Contact from "@/pages/contact";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/contact" component={Contact} />
+      {/* <Route path="/contact" component={Contact} /> */}
       <Route component={NotFound} />
     </Switch>
   );
